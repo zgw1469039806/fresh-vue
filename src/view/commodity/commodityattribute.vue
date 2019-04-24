15d0e00fa206f07755c359a2514eb76819770a75
tfyv587 <2247589910@qq.com>
2019-04-24 08:54:57 +0800

    jiaheng.guo 2019-04-24 8:54 页面完善

--- src/view/commodity/commodityattribute.vue
+++ src/view/commodity/commodityattribute.vue
@@ -106,10 +106,13 @@
                 <el-form :model="ruleForm" inline :rules="rules" ref="ruleForm" label-width="100px"
                          class="demo-ruleForm">
                     <div class="spxx">
-                        <el-form-item label="活动名称" prop="name">
+                        <div class="b1title">
+                            <span>商品基本信息</span>
+                        </div>
+                        <el-form-item label="商品名称" prop="name">
                             <el-input v-model="ruleForm.name"></el-input>
                         </el-form-item>
-                        <el-form-item label="商品分类">
+                        <el-form-item label="商品分类" prop="type">
                             <el-select v-model="ruleForm.type" filterable multiple placeholder="请选择">
                                 <el-option
                                         v-for="item in options"
@@ -119,55 +122,38 @@
                                 </el-option>
                             </el-select>
                         </el-form-item>
-                        <el-form-item label="单位">
-                            <el-input></el-input>
+                        <el-form-item label="单位" prop="dw">
+                            <el-input v-model="ruleForm.dw"></el-input>
                         </el-form-item>
-                        <el-form-item label="简介">
-                            <el-input></el-input>
+                        <el-form-item label="简介" prop="jj">
+                            <el-input v-model="ruleForm.jj"></el-input>
                         </el-form-item>
                     </div>
                     <div class="jgxx">
-                        <el-form-item label="活动时间" required>
-                            <el-col :span="11">
-                                <el-form-item prop="date1">
-                                    <el-date-picker type="date" placeholder="选择日期" v-model="ruleForm.date1"
-                                                    style="width: 100%;"></el-date-picker>
-                                </el-form-item>
-                            </el-col>
-                            <el-col class="line" :span="2">-</el-col>
-                            <el-col :span="11">
-                                <el-form-item prop="date2">
-                                    <el-time-picker placeholder="选择时间" v-model="ruleForm.date2"
-                                                    style="width: 100%;"></el-time-picker>
-                                </el-form-item>
-                            </el-col>
+                        <div class="b1title"><span>商品价格信息</span></div>
+
+                        <el-form-item label="进货价" prop="jhj">
+                            <el-input v-model="ruleForm.jhj"></el-input>
                         </el-form-item>
-                        <el-form-item label="即时配送" prop="delivery">
-                            <el-switch v-model="ruleForm.delivery"></el-switch>
+                        <el-form-item label="零售价" prop="lsj">
+                            <el-input v-model="ruleForm.lsj"></el-input>
                         </el-form-item>
-                        <el-form-item label="活动性质" prop="type">
-                            <el-checkbox-group v-model="ruleForm.type">
-                                <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
-                                <el-checkbox label="地推活动" name="type"></el-checkbox>
-                                <el-checkbox label="线下主题活动" name="type"></el-checkbox>
-                                <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
-                            </el-checkbox-group>
+                        <el-form-item label="批发价" prop="pfj">
+                            <el-input v-model="ruleForm.pfj"></el-input>
                         </el-form-item>
-                        <el-form-item label="特殊资源" prop="resource">
-                            <el-radio-group v-model="ruleForm.resource">
-                                <el-radio label="线上品牌商赞助"></el-radio>
-                                <el-radio label="线下场地免费"></el-radio>
-                            </el-radio-group>
+                        <el-form-item label="配送价" prop="psj">
+                            <el-input v-model="ruleForm.psj"></el-input>
                         </el-form-item>
-                        <el-form-item label="活动形式" prop="desc">
-                            <el-input type="textarea" v-model="ruleForm.desc"></el-input>
-                        </el-form-item>
-                        <el-form-item>
-                            <el-button type="primary" @click="submitForm('ruleForm')">立即创建</el-button>
-                            <el-button @click="resetForm('ruleForm')">重置</el-button>
+                        <el-form-item label="最低价" prop="zdj">
+                            <el-input v-model="ruleForm.zdj"></el-input>
                         </el-form-item>
                     </div>
+                    <div style="float: right;">
+                        <el-button>取消</el-button>
+                        <el-button type="success" @click="submitForm('ruleForm')">添加</el-button>
+                    </div>
                 </el-form>
+                <div style="clear: both;"></div>
             </el-dialog>
         </div>
     </div>
@@ -207,37 +193,43 @@
                     }
                 ],
                 ruleForm: {
-                    name: '',
-                    region: '',
-                    date1: '',
-                    date2: '',
-                    delivery: false,
-                    type: [],
-                    resource: '',
-                    desc: ''
+                    name: '',//商品名称
+                    type: [],//商品分类
+                    dw: '',//商品单位
+                    jj: '',//商品简介
+                    jhj: '',//进货价
+                    lsj: '',//零售价
+                    pfj: '0',//批发价
+                    psj: '0',//配送价,
+                    zdj: '0'//最低价
                 },
                 rules: {
                     name: [
-                        {required: true, message: '请输入活动名称', trigger: 'blur'},
-                        {min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur'}
-                    ],
-                    region: [
-                        {required: true, message: '请选择活动区域', trigger: 'change'}
-                    ],
-                    date1: [
-                        {type: 'date', required: true, message: '请选择日期', trigger: 'change'}
-                    ],
-                    date2: [
-                        {type: 'date', required: true, message: '请选择时间', trigger: 'change'}
+                        {required: true, message: '请输入商品名称', trigger: 'blur'}
                     ],
                     type: [
-                        {type: 'array', required: true, message: '请至少选择一个活动性质', trigger: 'change'}
+                        {type: 'array', required: true, message: '请选择商品类型', trigger: 'change'}
                     ],
-                    resource: [
-                        {required: true, message: '请选择活动资源', trigger: 'change'}
+                    dw: [
+                        {required: true, message: '请输入商品单位', trigger: 'blur'}
                     ],
-                    desc: [
-                        {required: true, message: '请填写活动形式', trigger: 'blur'}
+                    jj:[
+                        {required: true, message: '请输入商品简介', trigger: 'blur'}
+                    ],
+                    jhj:[
+                        {required: true, message: '请输入商品进货价', trigger: 'blur'}
+                    ],
+                    lsj:[
+                        {required: true, message: '请输入商品零售价', trigger: 'blur'}
+                    ],
+                    pfj:[
+                        {required: true, message: '请输入商品批发价', trigger: 'blur'}
+                    ],
+                    psj:[
+                        {required: true, message: '请输入商品配送价', trigger: 'blur'}
+                    ],
+                    zdj:[
+                        {required: true, message: '请输入商品最低价', trigger: 'blur'}
                     ]
                 },
                 Form: {
@@ -294,8 +286,18 @@
             },
             close: function () {
                 this.dialogVisible = false;
+            },
+            submitForm(formName) {
+                this.$refs[formName].validate((valid) => {
+                    if (valid) {
+                        alert('submit!');
+                    } else {
+                        return false;
+                    }
+                });
             }
-        }
+        },
+
     }
 </script>
 
@@ -321,12 +323,29 @@
     }
 
     .spxx {
-        border: 1px solid red;
+        border: 1px solid #c4c6cc;
         margin: 10px;
+        border-radius: 5px;
+    }
+
+    .spxx:hover, .jgxx:hover {
+        box-shadow: rgba(0, 0, 0, 0.1) 0px 0px 4px 4px;
     }
 
     .jgxx {
+        border-radius: 5px;
+        border: 1px solid #c4c6cc;
         margin: 10px;
-        border: 1px solid red;
+    }
+
+    .b1title {
+        border-bottom: 1px solid #c4c6cc;
+        height: 50px;
+        line-height: 50px;
+        margin-bottom: 15px;
+    }
+
+    .b1title span {
+        margin-left: 15px;
     }
 </style>
